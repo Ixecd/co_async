0e721e745402ce7838339ef9e8f623112e548319
Krewoe <2192629378@qq.com>
2024-07-26 17:35:21 +0800

    详细介绍

--- example/when_all.cc
+++ example/when_all.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <tuple>
 #include <coroutine>
 #include <co_async/qc.hpp>
 #include <co_async/task.hpp>
@@ -28,12 +29,53 @@ Task<void> hello2() {
     co_return;
 }
 
+Task<int> fiber1() {
+    DEBUG(fiber1begin);
+    int fiber1Var = 100;
+    PRINT(fiber1Var);
+    co_return fiber1Var + 100;
+}
 
-int main() {
+Task<double> fiber2() {
+    DEBUG(fiber2begin);
+    double fiber2Var = 314;
+    PRINT(fiber2Var);
+    co_return fiber2Var * 2;
+}
 
+Task<void> fiber3() {
+    DEBUG(fiber3);
+
+    co_return;
+}
+
+
+void testHello() {
     auto t1 = hello1();
     auto t2 = hello2();
     auto t = when_all(t1, t2);
+    getLoop()->addTask(t);
+    getLoop()->process();
+}
+
+Task<void> testVar() {
+    auto t1 = fiber1();
+    auto t2 = fiber2();
+    auto t3 = fiber3();
+    // tuple结构化绑定
+    // when_all本质上是一个模板类型为tuple的Task
+    // 
+    auto [x, y, z] = co_await when_all(t1, t2, t3);
+    PRINT(x);
+    PRINT(y);
+
+    co_return;
+}
+
+
+int main() {
+
+    auto t = testVar();
     getLoop()->addTask(t);
     getLoop()->process();
 
