40a70a54445a12a5f998e1fb16e19ef742990cba
Krewoe <2192629378@qq.com>
2024-07-27 16:07:23 +0800

    co_await

--- example/co_await_leftone.cc
+++ example/co_await_leftone.cc
@@ -1,13 +1,13 @@
 #include <chrono>
 #include <tuple>
 #include <coroutine>
-#include <co_async/qc.hpp>
+#include <utilities/qc.hpp>
 #include <co_async/task.hpp>
 #include <co_async/when_all.hpp>
 #include <co_async/concepts.hpp>
-#include <co_async/uninitialized.hpp>
-#include <co_async/sleep_awaiter.hpp>
-#include <co_async/non_void_helper.hpp>
+#include <co_async/timerLoop.hpp>
+#include <utilities/uninitialized.hpp>
+#include <utilities/non_void_helper.hpp>
 #include <co_async/return_previous.hpp>
 #include <co_async/previous_awaiter.hpp>
 
