04e6e8c5c308a01a72e73ae641f857b437c2a6c2
Krewoe <2192629378@qq.com>
2024-07-27 16:17:52 +0800

    testWhenAny

--- example/when_any.cc
+++ example/when_any.cc
@@ -2,15 +2,15 @@
 #include <tuple>
 #include <variant>
 #include <coroutine>
-#include <co_async/qc.hpp>
+#include <utilities/qc.hpp>
 #include <co_async/task.hpp>
 #include <co_async/when_any.hpp>
 #include <co_async/when_all.hpp>
 #include <co_async/concepts.hpp>
 #include <co_async/timerLoop.hpp>
 #include <co_async/scheduler.hpp>
-#include <co_async/uninitialized.hpp>
-#include <co_async/non_void_helper.hpp>
+#include <utilities/uninitialized.hpp>
+#include <utilities/non_void_helper.hpp>
 #include <co_async/return_previous.hpp>
 #include <co_async/previous_awaiter.hpp>
 
@@ -19,14 +19,14 @@ using namespace std::chrono_literals;
 
 Task<int> hello1() {
     DEBUG(hello1BeginSleep);
-    co_await sleep_for(1s); // 1s 等价于 std::chrono::seconds(1)
+    co_await sleep_for(getTimerLoop(), 1s); // 1s 等价于 std::chrono::seconds(1)
     DEBUG(hello1EndSleep);
     co_return 1;
 }
 
 Task<int> hello2() {
     DEBUG(hello2BeginSleep);
-    co_await sleep_for(2s); // 2s 等价于 std::chrono::seconds(2)
+    co_await sleep_for(getTimerLoop(), 2s); // 2s 等价于 std::chrono::seconds(2)
     DEBUG(hello2EndSleep);
     co_return 2;
 }
@@ -46,5 +46,6 @@ int main() {
     auto t = hello();
     getLoop()->addTask(t);
     getLoop()->process();
+    getTimerLoop().process();
     return 0;
 }
