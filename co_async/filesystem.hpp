44c204f4ad850fc70405c609fc0507bbcab07783
Ixecd <2192629378@qq.com>
2024-08-02 21:24:10 +0800

    filesystem

---
+++ co_async/filesystem.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <coroutine>
+#include <chrono>
+#include <cstdint>
+#include <utility>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <span>
+#include <filesystem>
+#include <unistd.h>
+#include <fcntl.h>
+#include "task.hpp"
+#include "ioLoop.hpp"
+
+namespace co_async {
+
+enum class OpenMode : int {
+    Read = O_RDONLY,
+    Write = O_WRONLY | O_TRUNC | O_CREAT,
+    ReadWrite = O_RDWR | O_CREAT,
+    Append = O_WRONLY | O_APPEND | O_CREAT,
+};
+
+inline Task<AsyncFile> open_fs_file(IoLoop &loop, std::filesystem::path path, OpenMode mode, mode_t access = 0644) {
+    int oflags = (int)mode;
+    /* oflags |= O_NONBLOCK; */ // TODO: nonblockfsfilesviaiouring!
+    int res = checkError(open(path.c_str(), oflags, access));
+    AsyncFile file(res);
+    co_return file;
+}
+
+}
